c0ec242e04fefc33dd46e0ed7968ed0ba6c5de03
Chloemlla <happyclovo@gmail.com>
2026-08-01 14:01:42 +0800

    Remove invalid core plugin apply false

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,5 +20,4 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlin.android) apply false
-    id("maven-publish") apply false
 }
